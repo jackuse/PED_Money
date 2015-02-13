ea1e6f30353c737e608977695e2267ddf00443f2
Jordan Aupetit <aupetit.jordan@gmail.com>
2015-02-13 18:06:26 +0100

    List operation static

--- client/app/components/operation/operationController.js
+++ client/app/components/operation/operationController.js
@@ -9,7 +9,30 @@
 
     function OperationController($scope) {
 
-        $scope.test = "Coucou operation";
+        $scope.operations = [
+            {
+                value: -50,
+                thirdParty: "Auchan",
+                description: "Règlement des courses",
+                type: "Chèque",
+                checked: false,
+                dateOperation: "20/01/2015",
+                datePrelevement: "25/01/2015",
+                categoryId: "54684654dqs",
+                subOperations: []
+            },
+            {
+                value: 100,
+                thirdParty: "Maman",
+                description: "Argent de poche",
+                type: "Virement",
+                checked: true,
+                dateOperation: "01/01/2015",
+                datePrelevement: "12/01/2015",
+                categoryId: "eza5484654dqs",
+                subOperations: []
+            }
+        ]
 
     }
 
