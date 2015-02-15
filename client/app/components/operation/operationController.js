10154cebef4c9badc8b5d39e6c097712a99f59bb
Jordan Aupetit <aupetit.jordan@gmail.com>
2015-02-15 20:04:13 +0100

    MAJ View operation -- Add table

--- client/app/components/operation/operationController.js
+++ client/app/components/operation/operationController.js
@@ -6,36 +6,7 @@
         .module('controllers')
         .controller('OperationController', ['$scope', 'OperationResource', function($scope, OperationResource) {
 
-
-
-
-            $scope.operations = [
-                {
-                    value: -50,
-                    thirdParty: "Auchan",
-                    description: "Règlement des courses",
-                    type: "Chèque",
-                    checked: false,
-                    dateOperation: "20/01/2015",
-                    datePrelevement: "25/01/2015",
-                    categoryId: "54684654dqs",
-                    subOperations: []
-                },
-                {
-                    value: 100,
-                    thirdParty: "Maman",
-                    description: "Argent de poche",
-                    type: "Virement",
-                    checked: true,
-                    dateOperation: "01/01/2015",
-                    datePrelevement: "12/01/2015",
-                    categoryId: "eza5484654dqs",
-                    subOperations: []
-                }
-            ]
-
-
-            var test = {
+            var op1 = {
                 value: -50,
                 thirdParty: "Auchan",
                 description: "Règlement des courses",
@@ -47,11 +18,25 @@
                 subOperations: []
             }
 
+            var op2 = {
+                value: 100,
+                thirdParty: "Maman",
+                description: "Argent de poche",
+                type: "Virement",
+                checked: true,
+                dateOperation: "01/01/2015",
+                datePrelevement: "12/01/2015",
+                categoryId: "eza5484654dqs",
+                subOperations: []
+            }
 
-            OperationResource.add(test);
+
+            //OperationResource.add(op1);
+            //OperationResource.add(op2);
 
             OperationResource.getAll().$promise.then(function(operations){
-                console.log(operations);
+                console.log(operations)
+                $scope.operations = operations
             })
 
 
